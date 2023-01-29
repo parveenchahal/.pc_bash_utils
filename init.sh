d25bd4c2f15c0762f04743850851fdd0ce45cf61
Parveen Chahal <mail.chahal@gmail.com>
2023-01-29 02:12:08 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,14 @@
 alias update-pc-bash-utils='cd ~/.pc_bash_utils && git pull && cd - && source ~/.pc_bash_utils/init.sh && bash'
 
-source ~/.pc_bash_init/common-jump.sh
-source ~/.pc_bash_init/kubectl/init.sh
-source ~/.pc_bash_init/git/init.sh
-source ~/.pc_bash_init/openssl.sh
-source ~/.pc_bash_init/internal_utils.sh
-source ~/.pc_bash_init/open.sh
-source ~/.pc_bash_init/run.sh
-source ~/.pc_bash_init/default-options-for-commands.sh
-source ~/.pc_bash_init/regex_utils.sh
-source ~/.pc_bash_init/date_utils.sh
-source ~/.pc_bash_init/clipboard_utils.sh
-source ~/.pc_bash_init/docker.sh
+source ~/.pc_bash_utils/common-jump.sh
+source ~/.pc_bash_utils/kubectl/init.sh
+source ~/.pc_bash_utils/git/init.sh
+source ~/.pc_bash_utils/openssl.sh
+source ~/.pc_bash_utils/internal_utils.sh
+source ~/.pc_bash_utils/open.sh
+source ~/.pc_bash_utils/run.sh
+source ~/.pc_bash_utils/default-options-for-commands.sh
+source ~/.pc_bash_utils/regex_utils.sh
+source ~/.pc_bash_utils/date_utils.sh
+source ~/.pc_bash_utils/clipboard_utils.sh
+source ~/.pc_bash_utils/docker.sh

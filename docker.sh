0eacaac1a2adcbc9f4a913efe95df6803f060401
Parveen Chahal <mail.chahal@gmail.com>
2023-03-06 03:21:13 -0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,3 +1,8 @@
 function docker-remove-images-all() {
   docker image ls | awk '{print $3}' | grep -v IMAGE | xargs docker rmi
 }
+
+function docker-stop-and-remove-container() {
+  docker stop $1
+  docker rm $1
+}

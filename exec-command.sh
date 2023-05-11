be2da89bcac5dee535e3b383710f8676a477840a
Parveen Chahal <mail.chahal@gmail.com>
2023-05-10 17:16:17 -0700

    Create exec-command.sh

---
+++ exec-command.sh
@@ -0,0 +1,17 @@
+alias exec-command="~/.exec-command.sh"
+
+function edit-exec-command() {
+  local cmd="vim"
+  local file_path="~/.exec-command.sh"
+  if [ ! -z "$1" ]
+  then
+    cmd="$1"
+  fi
+  if [ ! -f "$file_path" ]
+  then
+    touch "$file_path"
+    chmod +x "$file_path"
+  fi
+  cmd="$cmd $file_path"
+  eval "$cmd"
+}

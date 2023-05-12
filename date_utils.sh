5d8c7de20927307a5b068d219339ffc9297a25c7
Parveen Chahal <mail.chahal@gmail.com>
2023-05-11 17:59:17 -0700

    Update date_utils.sh

--- date_utils.sh
+++ date_utils.sh
@@ -1,11 +1,22 @@
 function epoch_to_date() {
   local d=$1
   echo "UTC: $(date -d @$d -u)"
-  echo "PST: $(date -d @$d)"
+  echo "Local: $(date -d @$d)"
 }
 
 function epoch_to_date_ms() {
   local d=$(($1 / 1000))
   echo "UTC: $(date -d @$d -u)"
-  echo "PST: $(date -d @$d)"
+  echo "Local: $(date -d @$d)"
+}
+
+function date_to_epoch() {
+  if [ ! -z "$1" ]
+  then
+    echo "UTC: $(date -u -d "$1") -> $(date -u -d "$1" +"%s") epoch sec"
+    echo "Local: $(date -d "$1") -> $(date -d "$1" +"%s") epoch sec"
+    return 0
+  fi
+  echo "UTC: $(date -u) -> $(date -u +"%s") epoch sec"
+  echo "Local: $(date) -> $(date +"%s") epoch sec"
 }

c86dd7e2d700ac6bebb7a570017cdbe3667f8028
Parveen Chahal <mail.chahal@gmail.com>
2023-09-28 19:57:20 -0700

    Update conversion.sh

--- conversion.sh
+++ conversion.sh
@@ -10,3 +10,7 @@ function hex_to_bin() {
   local input=$(tr '[a-z]' '[A-Z]' <<< $1)
   echo "obase=2; ibase=16; $input" | bc
 }
+
+ alias bin-to-hexdump='xxd -ps'
+ 
+ alias hexdump-to-bin='xxd -r -p'

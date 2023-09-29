ed24bf8d39c0f39ece14a9d218094b12c5a46b90
Parveen Chahal <mail.chahal@gmail.com>
2023-09-28 19:46:25 -0700

    Update conversion.sh

--- conversion.sh
+++ conversion.sh
@@ -5,3 +5,8 @@ function hex_to_dec() {
 function dec_to_hex() {
   printf "%x\n" "$1"
 }
+
+function hex_to_bin() {
+  local input=$(tr '[a-z]' '[A-Z]' <<< $1)
+  echo "obase=2; ibase=16; $input" | bc
+}

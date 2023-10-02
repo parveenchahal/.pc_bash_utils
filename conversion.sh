6742f0dd33798222adafb6d3e03dc280e6c75211
Parveen Chahal <mail.chahal@gmail.com>
2023-10-01 20:49:32 -0700

    Update conversion.sh

--- conversion.sh
+++ conversion.sh
@@ -12,6 +12,14 @@ function num-hex-to-bin() {
   #echo "obase=2; ibase=16; $input" | bc
 }
 
+function num-oct-to-dec() {
+  echo $((8#$1))
+}
+
+function num-dec-to-oct() {
+  printf "%o\n" "$1"
+}
+
  alias bin-to-hexdump='xxd -ps'
  
  alias hexdump-to-bin='xxd -r -p'

dcb2e6764813093a06ad577c46015c200606b7a2
Parveen Chahal <mail.chahal@gmail.com>
2023-10-23 12:04:26 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 alias update-pc-bash-utils='cd ~/.pc_bash_utils && git pull && cd - && source ~/.pc_bash_utils/init.sh'
 
+source ~/.pc_bash_utils/internal_utils.sh
 source ~/.pc_bash_utils/python.sh
 source ~/.pc_bash_utils/common-jump.sh
 source ~/.pc_bash_utils/kubectl/init.sh
 source ~/.pc_bash_utils/git/init.sh
 source ~/.pc_bash_utils/openssl.sh
-source ~/.pc_bash_utils/internal_utils.sh
 source ~/.pc_bash_utils/open.sh
 source ~/.pc_bash_utils/run.sh
 source ~/.pc_bash_utils/default-options-for-commands.sh

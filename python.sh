031196a17c08b33af569a47538d3b4095ace53d0
Parveen Chahal <mail.chahal@gmail.com>
2023-11-02 10:30:11 -0700

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,3 +1,16 @@
+alias run-test-py="python3 ~/.py-test.py"
+
+complete -W "-e --editor" edit-test-py
+function edit-test-py() {
+  pbu_extract_arg 'e' 'editor' "$@" || REPLY=vim
+  local editor="$REPLY"
+  
+  local file_path=~/.py-test.py
+  pbu_is_file_exist $file_path || touch $file_path
+  cmd="$editor $file_path"
+  eval "$cmd"
+}
+
 function py-exec() {
 cmd=$(
 cat<<EOF

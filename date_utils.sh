e939fe850e4b688d81e6cceb1e6c65ce66af3052
Parveen Chahal <mail.chahal@gmail.com>
2023-11-02 07:20:05 -0700

    Update date_utils.sh

--- date_utils.sh
+++ date_utils.sh
@@ -16,6 +16,12 @@ function date_from_epoch_microseconds() {
   echo "Local: $(date -d @$d)"
 }
 
+function date_from_epoch_nanoseconds() {
+  local d=$(($1 / 1000000000))
+  echo "UTC: $(date -d @$d -u)"
+  echo "Local: $(date -d @$d)"
+}
+
 function date_to_epoch() {
   if [ ! -z "$1" ]
   then

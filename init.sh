c5f41944134dc304116ef1935d118d73c1f08912
Parveen Chahal <mail.chahal@gmail.com>
2023-11-28 05:23:15 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,40 +1,5 @@
-if [ ! -f ~/.pc_bash_utils/.lastupdatecheck ]
-then
-  echo 0 > ~/.pc_bash_utils/.lastupdatecheck
-fi
+. ~/.pc_bash_utils/update_management.sh
 
-if [ ! -f ~/.pc_bash_utils/.updatepending ]
-then
-  echo 0 > ~/.pc_bash_utils/.updatepending
-fi
-
-hours2past="$(date -d "-1 day" +"%s")"
-lastupdatecheck=$(cat ~/.pc_bash_utils/.lastupdatecheck)
-if [ "$lastupdatecheck" != "disabled" ]
-then
-  if [ "$lastupdatecheck" -lt "$hours2past" ]
-  then
-    cd ~/.pc_bash_utils
-    updatePending=$(cat ~/.pc_bash_utils/.updatepending)
-    if [ "$updatePending" == "0" ]
-    then
-      changed=0
-      echo "Checking for new updates for pc_bash_utils" && git remote update > /dev/null 2>&1 && git status -uno | grep -q 'Your branch is behind' && changed=1
-      echo "$changed" > ~/.pc_bash_utils/.updatepending
-    fi
-    updatePending=$(cat ~/.pc_bash_utils/.updatepending)
-    if [ "$updatePending" == "1" ]
-    then
-      echo "Please run \"update-pc-bash-utils\" to update latest bash utils"
-    else
-      date +"%s" > ~/.pc_bash_utils/.lastupdatecheck
-    fi
-    cd - > /dev/null 2>&1
-  fi
-fi
-
-
-alias update-pc-bash-utils='cd ~/.pc_bash_utils && git pull && cd - > /dev/null 2>&1 && echo "0" > ~/.pc_bash_utils/.updatepending && source ~/.pc_bash_utils/init.sh'
 source ~/.pc_bash_utils/default-options-for-commands.sh
 source ~/.pc_bash_utils/parse_arguments.sh
 source ~/.pc_bash_utils/internal_utils.sh

f8dc3cfe54187b6d6822a3bb76faeb46eb1025e5
Parveen Chahal <mail.chahal@gmail.com>
2023-11-28 02:47:41 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,21 +1,29 @@
-if [ -f ~/.pc_bash_utils/.lastupdate ]
+if [ ! -f ~/.pc_bash_utils/.lastupdate ]
 then
-  hours2past="$(date -d "-2 hours" +"%s")"
-  lastUpdate=$(cat ~/.pc_bash_utils/.lastupdate)
-  if [ "$lastUpdate" != "disabled" ]
-  then
-    if [ "$lastUpdate" -lt "$hours2past" ]
-    then
-      cd ~/.pc_bash_utils
-      git remote update > /dev/null 2>&1 && git status -uno | grep -q 'Your branch is behind' && echo "Please run \"update-pc-bash-utils\" to update latest bash utils"
-      cd - > /dev/null 2>&1
-    fi
-  fi
-else
   date +"%s" > ~/.pc_bash_utils/.lastupdate
 fi
 
-alias update-pc-bash-utils='cd ~/.pc_bash_utils && git pull && cd - && ([ "$(cat ~/.pc_bash_utils/.lastupdate)" == "disabled" ] || (date +"%s" > ~/.pc_bash_utils/.lastupdate)) && source ~/.pc_bash_utils/init.sh'
+hours2past="$(date -d "-2 hours" +"%s")"
+lastUpdate=$(cat ~/.pc_bash_utils/.lastupdate)
+if [ "$lastUpdate" != "disabled" ]
+then
+  if [ "$lastUpdate" -lt "$hours2past" ]
+  then
+    cd ~/.pc_bash_utils
+    changed=0
+    git remote update > /dev/null 2>&1 && git status -uno | grep -q 'Your branch is behind' && changed=1
+    if [ "$changed" == "1" ]
+    then
+      echo "Please run \"update-pc-bash-utils\" to update latest bash utils"
+    else
+      date +"%s" > ~/.pc_bash_utils/.lastupdate
+    fi
+    cd - > /dev/null 2>&1
+  fi
+fi
+
+
+alias update-pc-bash-utils='cd ~/.pc_bash_utils && git pull && cd - > /dev/null 2>&1 && source ~/.pc_bash_utils/init.sh'
 source ~/.pc_bash_utils/default-options-for-commands.sh
 source ~/.pc_bash_utils/parse_arguments.sh
 source ~/.pc_bash_utils/internal_utils.sh

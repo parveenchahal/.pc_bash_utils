314c1c222805c5111de7d66238b897fe28fa32a8
Parveen Chahal <mail.chahal@gmail.com>
2023-11-27 22:01:14 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ if [ -f ~/.pc_bash_utils/.lastupdate ]
 then
   days30past="$(date -d "-1 minute" +"%s")"
   lastUpdate=$(cat ~/.pc_bash_utils/.lastupdate)
-  if [ ! "$lastUpdate" == "disabled" ]
+  if [ "$lastUpdate" != "disabled" ]
   then
     if [ "$lastUpdate" -lt "$days30past" ]
     then
@@ -13,7 +13,7 @@ else
   date +"%s" > ~/.pc_bash_utils/.lastupdate
 fi
 
-alias update-pc-bash-utils='cd ~/.pc_bash_utils && git pull && cd - && ([ ! $(cat ~/.pc_bash_utils/.lastupdate) == "disabled" ] || (date +"%s" > ~/.pc_bash_utils/.lastupdate)) && source ~/.pc_bash_utils/init.sh'
+alias update-pc-bash-utils='cd ~/.pc_bash_utils && git pull && cd - && ([ "$(cat ~/.pc_bash_utils/.lastupdate)" == "disabled" ] || (date +"%s" > ~/.pc_bash_utils/.lastupdate)) && source ~/.pc_bash_utils/init.sh'
 source ~/.pc_bash_utils/default-options-for-commands.sh
 source ~/.pc_bash_utils/parse_arguments.sh
 source ~/.pc_bash_utils/internal_utils.sh

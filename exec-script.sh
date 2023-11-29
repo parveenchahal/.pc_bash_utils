6c3359a56d6cc24083d0a62d4bf79fcca8d0b0d4
Parveen Chahal <mail.chahal@gmail.com>
2023-11-28 22:45:20 -0800

    Update exec-script.sh

--- exec-script.sh
+++ exec-script.sh
@@ -1,14 +1,37 @@
-alias exec-script='~/.exec-script.sh'
+function _pbu_create_exec_script_dir_if_not_created() {
+  if [ ! -d ~/.exec-script ]
+  then
+    mkdir ~/.exec-script
+  fi
+}
 
-complete -W "-e --editor" edit-exec-script
+complete -W "-n --name" exec-script
+function exec-script() {
+  pbu_extract_arg 'n' 'name' "$@" || pbu_error_echo "-n/--name is required argument." || return 1
+  local name="$REPLY"
+  _pbu_create_exec_script_dir_if_not_created
+  cd ~/.exec-script
+  if [ ! -f "$name.sh" ]
+  then
+    eval "touch \"$name\""
+    eval "chmod +x \"$name.sh\""
+  fi
+  eval "bash \"$name.sh\""
+  cd -
+}
+
+complete -W "-n --name -e --editor" edit-exec-script
 function edit-exec-script() {
+  pbu_extract_arg 'n' 'name' "$@" || pbu_error_echo "-n/--name is required argument." || return 1
+  local name="$REPLY"
+  _pbu_create_exec_script_dir_if_not_created
   pbu_extract_arg 'e' 'editor' "$@" || REPLY=vim
   local editor="$REPLY"
-  if [ ! -f ~/.exec-script.sh ]
+  cd ~/.exec-script
+  if [ ! -f "$name.sh" ]
   then
-    touch ~/.exec-script.sh
-    chmod +x ~/.exec-script.sh
+    eval "touch \"$name\""
+    eval "chmod +x \"$name.sh\""
   fi
-  cmd="$editor ~/.exec-script.sh"
-  eval "$cmd"
+  eval "$editor \"~/.exec-script/$name.sh\""
 }

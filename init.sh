ce7c4959d2f68409f1082296445531581122dcb4
Parveen Chahal <mail.chahal@gmail.com>
2023-11-28 19:24:56 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -14,6 +14,6 @@ source ~/.pc_bash_utils/regex_utils.sh
 source ~/.pc_bash_utils/date_utils.sh
 source ~/.pc_bash_utils/copy.sh
 source ~/.pc_bash_utils/docker.sh
-source ~/.pc_bash_utils/exec-command.sh
+source ~/.pc_bash_utils/exec-script.sh
 source ~/.pc_bash_utils/conversion.sh
 

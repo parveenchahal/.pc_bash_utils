0847ae7ee6d88ac95080f9e79b0f1eba1b048fb6
Parveen Chahal <mail.chahal@gmail.com>
2023-11-30 01:14:16 -0800

    Update exec-shell-script.sh

--- exec-shell-script.sh
+++ exec-shell-script.sh
@@ -2,18 +2,18 @@ complete -W "--script-name" exec-bash-script
 function exec-bash-script() {
   pbu_extract_arg '' 'script-name' "$@" || pbu_error_echo "--script-name is required argument." || return 1
   local name="$REPLY"
-  pbu_create_dir_if_does_not_exist ~/.exec-script
-  cd ~/.exec-script
+  pbu_create_dir_if_does_not_exist ~/.bash-script
+  cd ~/.bash-script
   eval "\"./$name.sh\" \"$@\""
   cd - > /dev/null 2>&1
 }
 
 complete -W "--script-name --editor" edit-bash-script
 function edit-bash-script() {
-  local basePath="$(realpath ~/.exec-script)"
+  local basePath="$(realpath ~/.bash-script)"
   pbu_extract_arg '' 'script-name' "$@" || pbu_error_echo "--script-name is required argument." || return 1
   local name="$REPLY"
-  pbu_create_dir_if_does_not_exist ~/.exec-script
+  pbu_create_dir_if_does_not_exist ~/.bash-script
   pbu_extract_arg '' 'editor' "$@" || REPLY=vim
   local editor="$REPLY"
   if [ ! -f "$basePath/$name.sh" ]

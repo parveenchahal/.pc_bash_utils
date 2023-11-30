6193c47fdd9295d59b86f0c15bed8c8fcca3e831
Parveen Chahal <mail.chahal@gmail.com>
2023-11-30 01:01:56 -0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,13 +1,23 @@
-alias run-test-py="python3 ~/.py-test.py"
+complete -W "--script-name" exec-py-script
+function exec-py-script() {
+  pbu_extract_arg '' 'script-name' "$@" || pbu_error_echo "--script-name is required argument." || return 1
+  local name="$REPLY"
+  pbu_create_dir_if_does_not_exist ~/.py-script
+  cd ~/.py-script
+  eval "\"python3 ./$name.py\" \"$@\""
+  cd - > /dev/null 2>&1
+}
 
-complete -W "-e --editor" edit-test-py
-function edit-test-py() {
-  pbu_extract_arg 'e' 'editor' "$@" || REPLY=vim
+complete -W "--script-name --editor" edit-py-script
+function edit-py-script() {
+  local basePath="$(realpath ~/.py-script)"
+  pbu_extract_arg '' 'script-name' "$@" || pbu_error_echo "--script-name is required argument." || return 1
+  local name="$REPLY"
+  pbu_create_dir_if_does_not_exist ~/.py-script
+  pbu_extract_arg '' 'editor' "$@" || REPLY=vim
   local editor="$REPLY"
-  
-  pbu_is_file_exist ~/.py-test.py || touch ~/.py-test.py
-  cmd="$editor ~/.py-test.py"
-  eval "$cmd"
+  pbu_create_dir_if_does_not_exist "$basePath/$name.py"
+  eval "$editor \"$basePath/$name.py\""
 }
 
 function py-exec() {

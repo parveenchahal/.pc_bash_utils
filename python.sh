3381da5ff667b981099d2f3ce3e47ed4f3bb27b8
Parveen Chahal <mail.chahal@gmail.com>
2023-12-12 15:07:59 -0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -5,36 +5,39 @@ function pbu_is_py_installed() {
   return 0
 }
 
-complete -W "--script-name" exec-py-script
-function exec-py-script() {
-  pbu_extract_arg '' 'script-name' "$@" || pbu_error_echo "--script-name is required argument." || return 1
-  local name="$REPLY"
-  pbu_create_dir_if_does_not_exist ~/.py-script
-  pushd ~/.py-script > /dev/null
-  python3 "$name.py"
-  popd > /dev/null
-}
-
-complete -W "--script-name --editor" edit-py-script
-function edit-py-script() {
-  local basePath="$(realpath ~/.py-script)"
-  pbu_extract_arg '' 'script-name' "$@" || pbu_error_echo "--script-name is required argument." || return 1
-  local name="$REPLY"
-  pbu_create_dir_if_does_not_exist ~/.py-script
-  pbu_extract_arg '' 'editor' "$@" || REPLY=vim
-  local editor="$REPLY"
-  pbu_create_file_if_does_not_exist "$basePath/$name.py"
-  eval "$editor \"$basePath/$name.py\""
-}
-
-function py-exec() {
-cmd=$(
-cat<<EOF
-$@
-EOF
-)
-python3 -c "$cmd"
-}
-function py-print() {
-  py-exec "print($@)"
-}
+if pbu_is_py_installed;
+then
+  complete -W "--script-name" exec-py-script
+  function exec-py-script() {
+    pbu_extract_arg '' 'script-name' "$@" || pbu_error_echo "--script-name is required argument." || return 1
+    local name="$REPLY"
+    pbu_create_dir_if_does_not_exist ~/.py-script
+    pushd ~/.py-script > /dev/null
+    python3 "$name.py"
+    popd > /dev/null
+  }
+  
+  complete -W "--script-name --editor" edit-py-script
+  function edit-py-script() {
+    local basePath="$(realpath ~/.py-script)"
+    pbu_extract_arg '' 'script-name' "$@" || pbu_error_echo "--script-name is required argument." || return 1
+    local name="$REPLY"
+    pbu_create_dir_if_does_not_exist ~/.py-script
+    pbu_extract_arg '' 'editor' "$@" || REPLY=vim
+    local editor="$REPLY"
+    pbu_create_file_if_does_not_exist "$basePath/$name.py"
+    eval "$editor \"$basePath/$name.py\""
+  }
+  
+  function py-exec() {
+  cmd=$(
+  cat<<EOF
+  $@
+  EOF
+  )
+  python3 -c "$cmd"
+  }
+  function py-print() {
+    py-exec "print($@)"
+  }
+fi

6b0eb885e424cfe78659a61ba19d385f25ce81e3
Parveen Chahal <mail.chahal@gmail.com>
2023-12-12 05:00:07 -0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,3 +1,10 @@
+alias pbu_py='python3'
+function pbu_is_py_installed() {
+  local out=$(pbu_py -c 'print("", end="")' 2>&1)
+  [ "$out" == "" ] || return 1
+  return 0
+}
+
 complete -W "--script-name" exec-py-script
 function exec-py-script() {
   pbu_extract_arg '' 'script-name' "$@" || pbu_error_echo "--script-name is required argument." || return 1

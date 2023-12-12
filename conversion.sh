ca20d43589cbc7b41f40922f939eb57c34b084ef
Parveen Chahal <mail.chahal@gmail.com>
2023-12-12 01:23:36 -0800

    Update conversion.sh

--- conversion.sh
+++ conversion.sh
@@ -14,14 +14,14 @@ function num-hex-to-bin() {
 
 function num-bin-to-hex() {
   py-exec "
-    b = '$1'
-    if (len(b) % 4) != 0:
-    b = ('0' * (4 - len(b) % 4)) + b
-    h = []
-    for i in range(0, len(b), 4):
-      x = b[i:i+4]
-      h.append(hex(int(x, base=2))[2:])
-    print(''.join(h))
+b = '$1'
+if (len(b) % 4) != 0:
+b = ('0' * (4 - len(b) % 4)) + b
+h = []
+for i in range(0, len(b), 4):
+  x = b[i:i+4]
+  h.append(hex(int(x, base=2))[2:])
+print(''.join(h))
   "
 }
 

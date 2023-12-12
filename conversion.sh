de16859cdf849d32b3a9c792341a3481209f5e46
Parveen Chahal <mail.chahal@gmail.com>
2023-12-12 15:13:34 -0800

    Update conversion.sh

--- conversion.sh
+++ conversion.sh
@@ -6,6 +6,31 @@ function num-dec-to-hex() {
   printf "%x\n" "$1"
 }
 
+
+
+function num-oct-to-dec() {
+  printf "%d\n" "0$1"
+}
+
+function num-dec-to-oct() {
+  printf "%o\n" "$1"
+}
+
+ alias bin-to-hexdump='xxd -ps'
+ 
+ alias hexdump-to-bin='xxd -r -p'
+
+if pbu_is_py_installed;
+then
+
+function num-oct-to-bin() {
+  py-print "bin(int(\"$1\", 8))[2:]"
+}
+
+function num-dec-to-bin() {
+  py-print "bin(int(\"$1\"))[2:]"
+}
+
 function num-hex-to-bin() {
   py-print "bin(int(\"$1\", 16))[2:]"
   #local input=$(tr '[a-z]' '[A-Z]' <<< $1)
@@ -25,22 +50,4 @@ print(''.join(h))
   "
 }
 
-function num-oct-to-dec() {
-  printf "%d\n" "0$1"
-}
-
-function num-dec-to-oct() {
-  printf "%o\n" "$1"
-}
-
-function num-oct-to-bin() {
-  py-print "bin(int(\"$1\", 8))[2:]"
-}
-
-function num-dec-to-bin() {
-  py-print "bin(int(\"$1\"))[2:]"
-}
-
- alias bin-to-hexdump='xxd -ps'
- 
- alias hexdump-to-bin='xxd -r -p'
+fi

46896e0a8a5012cac6bf4fab3d2759a8c5cedd1e
Parveen Chahal <mail.chahal@gmail.com>
2023-12-20 11:47:03 -0800

    Update parse_arguments.sh

--- parse_arguments.sh
+++ parse_arguments.sh
@@ -109,20 +109,6 @@ function pbu_extract_arg() {
 #   return 1
 # }
 
-function pbu_is_arg_present() {
-  local short_key="$1"
-  shift
-  local long_key="$1"
-  shift
-  pbu_extract_arg "$short_key" "$long_key" "$@" || return 1
-  return 0
-}
-
-function pbu_is_arg_not_present() {
-  pbu_is_arg_present "$@" || return 0
-  return 1
-}
-
 function pbu_is_arg_switch_enabled() {
   local short_key="$1"
   shift

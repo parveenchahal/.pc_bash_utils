1f33eb146803273dd8f14fe1c3810b69e409945a
Parveen Chahal <mail.chahal@gmail.com>
2023-12-24 14:01:09 -0800

    Create eval.sh

---
+++ eval.sh
@@ -0,0 +1,25 @@
+function pbu_format_string_for_eval() {
+  [[ "${#1}" -gt 20 ]]  && echo "\"$1\"" && return 0
+  [[ "$1" == *\ * ]]  && echo "\"$1\"" && return 0
+  [[ "$1" == *\&* ]]  && echo "\"$1\"" && return 0
+  echo "$1"
+}
+
+function pbu_eval_cmd() {
+  args=()
+  for x in "$@"
+  do
+    args+=( "$(pbu_format_string_for_eval "$x")" )
+  done
+  eval "${args[@]}"
+}
+
+function pbu_eval_cmd_with_echo() {
+  args=()
+  for x in "$@"
+  do
+    args+=( "$(pbu_format_string_for_eval "$x")" )
+  done
+  echo "Executing command: ${args[@]}"
+  pbu_eval_cmd "$@"
+}

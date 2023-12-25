62ef3c971f614a323a6dd59d7de444dfb4addaef
Parveen Chahal <mail.chahal@gmail.com>
2023-12-24 16:35:58 -0800

    Update openssl.sh

--- openssl.sh
+++ openssl.sh
@@ -9,7 +9,7 @@ function pbu_openssl_decode() {
   local inform=$REPLY
   
   cmd="openssl $type -inform $inform -in \"$file\" -text -noout"
-  pbu_eval_cmd "$cmd"
+  pbu_eval_cmd_with_echo "$cmd"
 }
 
 complete -d -f -W "-f --file --inform" openssl-decode-cert 

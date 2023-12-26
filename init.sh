0a8958d005168143ffd084d5beef43c4631e96e3
Parveen Chahal <mail.chahal@gmail.com>
2023-12-26 00:56:21 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,9 +2,6 @@ source ~/.pc_bash_utils/internal/init.sh
 . ~/.pc_bash_utils/update_management.sh
 
 source ~/.pc_bash_utils/default-options-for-commands.sh
-source ~/.pc_bash_utils/parse_arguments.sh
-source ~/.pc_bash_utils/eval.sh
-source ~/.pc_bash_utils/internal_utils.sh
 source ~/.pc_bash_utils/python.sh
 source ~/.pc_bash_utils/common-jump.sh
 source ~/.pc_bash_utils/kubectl/init.sh

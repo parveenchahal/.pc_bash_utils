65a52cdc71be2d9fd19a769f1e91939fd7ad5d06
Parveen Chahal <mail.chahal@gmail.com>
2023-12-26 01:00:07 -0800

    Update init.sh

--- internal/init.sh
+++ internal/init.sh
@@ -2,5 +2,5 @@
 
 source ~/.pc_bash_utils/internal/parse_arguments.sh
 source ~/.pc_bash_utils/internal/eval.sh
-source ~/.pc_bash_utils/internal/internal_utils.sh
+source ~/.pc_bash_utils/internal/utils.sh
 source ~/.pc_bash_utils/internal/sql.sh

7f0d472f2d6cd47730fbdc5d94b9c4c28c59d1a5
Parveen Chahal <mail.chahal@gmail.com>
2023-12-26 01:09:05 -0800

    Create tmp.sh

---
+++ tmp.sh
@@ -0,0 +1,31 @@
+complete -W '--editor' tmp
+function tmp() {
+  local ARGS=$(getopt -q -l "editor:" -- "" "$@")
+  eval set -- "$ARGS"
+
+  pbu_non_option_arg_from_getopt_formated "$@"
+  local path="$REPLY"
+  
+  if [ -z "$path" ]
+  then
+    cd /tmp
+    return 0
+  fi
+  
+  local path="/tmp/$path"
+  if [ -f "$path" ]
+  then
+    pbu_extract_arg '' 'editor' "$@" || REPLY="vim"
+    pbu_eval_cmd "$REPLY" "$path"
+    return 0
+  fi
+  
+  if [ -d "$path" ]
+  then
+    cd "$path"
+    return 0
+  fi
+  
+  pbu_error_echo "Invalid path."
+  return 1
+}

9584caa8d7b551d9051ff6a9884c704b6d010e88
Parveen Chahal <mail.chahal@gmail.com>
2023-12-26 01:10:59 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ source ~/.pc_bash_utils/internal/init.sh
 source ~/.pc_bash_utils/default-options-for-commands.sh
 source ~/.pc_bash_utils/python.sh
 source ~/.pc_bash_utils/cd.sh
+source ~/.pc_bash_utils/tmp.sh
 source ~/.pc_bash_utils/kubectl.sh
 source ~/.pc_bash_utils/git.sh
 source ~/.pc_bash_utils/openssl.sh

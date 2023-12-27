a600249af29536e4f8f6f68f93b9016612c8b7ce
Parveen Chahal <mail.chahal@gmail.com>
2023-12-27 05:02:52 -0800

    Update parse_arguments.sh

--- internal/parse_arguments.sh
+++ internal/parse_arguments.sh
@@ -1,48 +1,3 @@
-# function pbu_extract_arg() {
-#   local short_key="$1"
-#   shift
-#   local long_key="$1"
-#   shift
-  
-#   local ARGS=''
-  
-#   if [[ -z "$short_key" && -z "$long_key" ]]
-#   then
-#     echo 'No option provided to pbu_extract_arg'
-#     return 1
-#   elif [[ ! -z "$short_key" && ! -z "$long_key" ]]
-#   then
-#     ARGS=$(getopt -q -o "$short_key:" -l "$long_key:" -- "" "$@")
-#   elif [ ! -z "$short_key" ]
-#   then
-#     ARGS=$(getopt -q -o "$short_key:" -- "" "$@")
-#   elif [ ! -z "$long_key" ]
-#   then
-#     ARGS=$(getopt -q -l "$long_key:" -- "" "$@")
-#   fi
-  
-#   eval set -- "$ARGS"
-
-#   REPLY=()
-#   local found=0
-#   while true ; do
-#     case "$1" in
-#       "--$long_key")
-#           REPLY+=("$2"); found=1 ; shift 2 ;;
-#       "-$short_key")
-#           REPLY+=("$2"); found=1 ; shift 2 ;;
-#       --) shift ; break ;;
-#       *) echo "Internal error in argument parsing!"; return 1 ;;
-#     esac
-#   done
-#   if [ "$found" == 0 ]
-#   then
-#     REPLY=""
-#     return 1
-#   fi
-#   return 0
-# }
-
 function pbu_extract_arg() {
   local short_key="$1"
   shift
@@ -110,6 +65,67 @@ function pbu_extract_arg() {
   return 0
 }
 
+function pbu_is_switch_arg_enabled() {
+  local short_key="$1"
+  shift
+  local long_key="$1"
+  shift
+  pbu_extract_arg "$short_key" "$long_key" "$@" || return 1
+  local value="$REPLY"
+  [ "$value" == "false" ] && return 1
+  [ "$value" == "true" ] && return 0
+  return 1
+}
+
+
+
+
+# function pbu_extract_arg() {
+#   local short_key="$1"
+#   shift
+#   local long_key="$1"
+#   shift
+  
+#   local ARGS=''
+  
+#   if [[ -z "$short_key" && -z "$long_key" ]]
+#   then
+#     echo 'No option provided to pbu_extract_arg'
+#     return 1
+#   elif [[ ! -z "$short_key" && ! -z "$long_key" ]]
+#   then
+#     ARGS=$(getopt -q -o "$short_key:" -l "$long_key:" -- "" "$@")
+#   elif [ ! -z "$short_key" ]
+#   then
+#     ARGS=$(getopt -q -o "$short_key:" -- "" "$@")
+#   elif [ ! -z "$long_key" ]
+#   then
+#     ARGS=$(getopt -q -l "$long_key:" -- "" "$@")
+#   fi
+  
+#   eval set -- "$ARGS"
+
+#   REPLY=()
+#   local found=0
+#   while true ; do
+#     case "$1" in
+#       "--$long_key")
+#           REPLY+=("$2"); found=1 ; shift 2 ;;
+#       "-$short_key")
+#           REPLY+=("$2"); found=1 ; shift 2 ;;
+#       --) shift ; break ;;
+#       *) echo "Internal error in argument parsing!"; return 1 ;;
+#     esac
+#   done
+#   if [ "$found" == 0 ]
+#   then
+#     REPLY=""
+#     return 1
+#   fi
+#   return 0
+# }
+
+
 # function pbu_is_arg_present() {
 #   local short_key="$1"
 #   shift
@@ -146,15 +162,3 @@ function pbu_extract_arg() {
 #   done
 #   return 1
 # }
-
-function pbu_is_switch_arg_enabled() {
-  local short_key="$1"
-  shift
-  local long_key="$1"
-  shift
-  pbu_extract_arg "$short_key" "$long_key" "$@" || return 1
-  local value="$REPLY"
-  [ "$value" == "false" ] && return 1
-  [ "$value" == "true" ] && return 0
-  return 1
-}

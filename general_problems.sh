7c22d0b6aca14b4f6f3548992ef05012fd02b1d8
Parveen Chahal <mail.chahal@gmail.com>
2024-01-03 16:03:43 -0800

    Create general_problems.sh

---
+++ general_problems.sh
@@ -0,0 +1,19 @@
+if pbu.py.is_installed;
+then
+
+complete -W "-n -k" birthday-problem
+function birthday-problem() {
+  local n
+  local k
+  pbu.args.extract -s n: -o n -- "$@" || pbu.errors.echo "-n is required arg" || return
+  pbu.args.extract -s k: -o k -- "$@" || pbu.errors.echo "-k is required arg" || return
+  py-exec "
+from operator import mul
+from functools import reduce
+from itertools import repeat
+p = (1 - reduce(mul, (x / y for x, y in zip(range($n - $k + 1, $n + 1), repeat($n))), 1)) * 100
+print('{0:0.10f}%'.format(p))
+  "
+}
+
+fi

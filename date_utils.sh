34f2729bfa84b838d4ef3b709e043c66c4090269
Parveen Chahal <mail.chahal@gmail.com>
2025-02-22 03:23:50 -0800

    Update date_utils.sh

--- date_utils.sh
+++ date_utils.sh
@@ -1,10 +1,3 @@
-alias pbu.date='date'
-if [[ "$OSTYPE" == "darwin"* ]]
-then
-  # macOS
-  alias pbu.date='gdate'
-fi
-
 complete -W "--out-utc --nanoseconds --microseconds --milliseconds --seconds" date-from-epoch
 function date-from-epoch() {
 

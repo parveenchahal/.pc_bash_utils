4926bc57f51ff792da1c8b9b586d02776469b08c
Parveen Chahal <mail.chahal@gmail.com>
2025-02-23 00:27:30 -0800

    Update init.sh

--- configs/init.sh
+++ configs/init.sh
@@ -1,6 +1,11 @@
 function pbu.force-update-config-files() {
   update-pc-bash-utils
-  declare -a config_list=(".tmux.conf" ".vimrc")
+  
+  local tmux='.tmux.conf'
+  [[ "$OSTYPE" == "darwin"* ]] && tmux='.tmux-macos.conf'
+  cp ~/.pc_bash_utils/configs/$x ~/.tmux.conf
+  
+  declare -a config_list=(".vimrc")
   for x in "${config_list[@]}"
   do
     cp ~/.pc_bash_utils/configs/$x ~/

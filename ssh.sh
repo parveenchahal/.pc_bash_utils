87b1716d8bb7a4676b23ba79b94e5c1de2d0b3dd
Parveen Chahal <mail.chahal@gmail.com>
2025-03-01 00:56:40 -0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,13 +1,14 @@
 complete -W "-u --user -h --host -e --exit-master-connection -t --tmux" pssh
 function pssh() {
+  local args=( "$@" )
   local emc='false'
   local tm='false'
-  pbash.args.is_switch_arg_enabled -s e -l exit-master-connection -- "$@" && emc='true'
-  pbash.args.is_switch_arg_enabled -s t -l tmux -- "$@" && tm='true'
+  pbash.args.is_switch_arg_enabled -s e -l exit-master-connection -r args -- "${args[@]}" && emc='true'
+  pbash.args.is_switch_arg_enabled -s t -l tmux -r args -- "${args[@]}" && tm='true'
   local u=""
   local h=""
-  pbash.args.extract -s u: -l user: -o u -- "$@" || pbash.args.errors.echo "-u/--user is required" || return 1
-  pbash.args.extract -s h: -l host: -o h -- "$@" || pbash.args.errors.echo "-h/--host is required" || return 1
+  pbash.args.extract -s u: -l user: -o u -r args -- "${args[@]}" || pbash.args.errors.echo "-u/--user is required" || return 1
+  pbash.args.extract -s h: -l host: -o h -r args -- "${args[@]}" || pbash.args.errors.echo "-h/--host is required" || return 1
 
   if [ "$(hostname)" == "$h" ]
   then
@@ -17,14 +18,14 @@ function pssh() {
   
   if [ "$emc" == "true" ]
   then
-    ssh -Y -C -O exit "$u@$h"
+    ssh -Y -C -O exit "$u@$h" "${args[@]}"
     return 0
   fi
 
   if [ "$tm" == "true" ]
   then
-    ssh -Y -C "$u@$h" -t bash -i -c 'ptmux'
+    ssh -Y -C "$u@$h" "${args[@]}" -t bash -i -c 'ptmux'
     return 0
   fi
-  ssh -Y -C "$u@$h"
+  ssh -Y -C "$u@$h" "${args[@]}"
 }

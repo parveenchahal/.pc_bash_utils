e2577f5e875d2eac1914c9ed7bab75af06e7bc3c
Parveen Chahal <mail.chahal@gmail.com>
2025-03-05 02:36:40 -0800

    Update parse_arguments.sh

--- internal/parse_arguments.sh
+++ internal/parse_arguments.sh
@@ -1,8 +1,7 @@
 if [ ! -f /usr/local/bin/pbash-args.sh ] && [ ! -f $HOME/.local/bin/pbash-args.sh ]
 then
   echo 'Installing pbash-args.sh...'
-  echo 'It requires sudo access.'
-  curl -sL https://pbash.pcapis.com/args/install.sh | sudo bash
+  curl -sL https://pbash.pcapis.com/args/install.sh | sudo bash -s -- --user
 fi
 
 source pbash-args.sh || pbu.errors.echo "pbash-args.sh is not installed, pc_bash_utils can not be used."

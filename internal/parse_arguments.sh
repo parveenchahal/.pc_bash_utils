b0fe215cc807a14eac1698cc8f0e5ea244952aa5
Parveen Chahal <mail.chahal@gmail.com>
2025-03-05 03:03:49 -0800

    Update parse_arguments.sh

--- internal/parse_arguments.sh
+++ internal/parse_arguments.sh
@@ -4,4 +4,4 @@ then
   curl -sL https://pbash.pcapis.com/args/install.sh | bash -s -- --user
 fi
 
-source /usr/local/bin/pbash-args.sh || source $HOME/.local/bin/pbash-args.sh || pbu.errors.echo "pbash-args.sh is not installed, pc_bash_utils can not be used."
+source pbash-args.sh || source $HOME/.local/bin/pbash-args.sh || pbu.errors.echo "pbash-args.sh is not installed, pc_bash_utils can not be used."

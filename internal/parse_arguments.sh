231b3e19f085ae6313240b2aba77f8e06a327260
Parveen Chahal <mail.chahal@gmail.com>
2025-03-05 03:17:23 -0800

    Update parse_arguments.sh

--- internal/parse_arguments.sh
+++ internal/parse_arguments.sh
@@ -1,7 +1,19 @@
-if [ ! -f /usr/local/bin/pbash-args.sh ] && [ ! -f $HOME/.local/bin/pbash-args.sh ]
-then
-  echo 'Installing pbash-args.sh...'
-  curl -sL https://pbash.pcapis.com/args/install.sh | bash -s -- --user
-fi
-
-source pbash-args.sh || source $HOME/.local/bin/pbash-args.sh || pbu.errors.echo "pbash-args.sh is not installed, pc_bash_utils can not be used."
+function ___pbu_check_and_install() {
+  local installation_type=''
+  [ -f /usr/local/bin/pbash-args.sh ] || installation_type='system'
+  [ -f $HOME/.local/bin/pbash-args.sh ] || installation_type='user'
+  if [ -z "$installation_type" ]
+  then
+    echo 'Installing pbash-args.sh...'
+    curl -sL https://pbash.pcapis.com/args/install.sh | bash -s -- --user
+    installation_type='user'
+  fi
+  if [ "$installation_type" == "user" ]
+  then
+    source $HOME/.local/bin/pbash-args.sh || pbu.errors.echo "pbash-args.sh is not installed, pc_bash_utils can not be used."
+  fi
+  if [ "$installation_type" == "system" ]
+  then
+    source /usr/local/bin/pbash-args.sh || pbu.errors.echo "pbash-args.sh is not installed, pc_bash_utils can not be used."
+  fi
+}

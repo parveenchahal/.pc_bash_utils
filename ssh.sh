0e40168efd3bf465ad6ff415366163cc6cb962a5
Parveen Chahal <mail.chahal@gmail.com>
2025-03-06 15:51:49 -0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -5,13 +5,20 @@ function pssh() {
 
   local u=""
   local h=""
-  pbash.args.extract -s u: -l user: -o u -r args -- "${args[@]}" || pbash.args.errors.echo "-u/--user is required" || return 1
   pbash.args.extract -s h: -l host: -o h -r args -- "${args[@]}" || pbash.args.errors.echo "-h/--host is required" || return 1
+  pbash.args.extract -s u: -l user: -o u -r args -- "${args[@]}"
+
   if [ "$(hostname)" == "$h" ]
   then
     pbu.errors.echo "Client and server are same."
     return 1
   fi
+
+  local uh="$h"
+
+  [ -z "$u" ] || uh="$u@$uh"
+
+  ssh_args+=( "$uh" )
   
   pbash.args.is_switch_arg_enabled -s e -l exit-master-connection -r args -- "${args[@]}" && ssh_args+=( -O exit )
   pbash.args.is_switch_arg_enabled -l x11 -r args -- "${args[@]}" && ssh_args+=( -o "ForwardAgent yes" -Y -C )

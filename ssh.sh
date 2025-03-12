86bc74bf62741f2f16b8e758771519bd2912451a
Parveen Chahal <mail.chahal@gmail.com>
2025-03-12 15:50:26 -0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,4 @@
-complete -W "-u --user -h --host -e --exit-master-connection --tmux -p --x11" pssh
+complete -W "-u --user -h --host -e --exit-master-connection -p --x11" pssh
 function pssh() {
   local args=( "$@" )
   local ssh_args=()
@@ -22,8 +22,6 @@ function pssh() {
   
   pbash.args.is_switch_arg_enabled -s e -l exit-master-connection -r args -- "${args[@]}" && ssh_args+=( -O exit )
   pbash.args.is_switch_arg_enabled -l x11 -r args -- "${args[@]}" && ssh_args+=( -o "ForwardAgent yes" -Y -C )
-  local tmux_cmd=()
-  pbash.args.is_switch_arg_enabled -l tmux -r args -- "${args[@]}" && tmux_cmd+=( -t bash -i -c 'ptmux' )
   
-  pbu.eval.cmd_with_echo ssh "${ssh_args[@]}" "${args[@]}" "${tmux_cmd[@]}"
+  pbu.eval.cmd_with_echo ssh "${ssh_args[@]}" "${args[@]}"
 }

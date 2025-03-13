76b4553ad8bfc6fb53de10ae84571057943bb630
Parveen Chahal <mail.chahal@gmail.com>
2025-03-12 18:14:56 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,21 +1,26 @@
-source ~/.pc_bash_utils/internal/init.sh
+if [[ "${BASH_VERSINFO[0]}" -ge 5 ]]
+then
+  source ~/.pc_bash_utils/internal/init.sh
 
-source ~/.pc_bash_utils/eval.sh
-source ~/.pc_bash_utils/default-options-for-commands.sh
-source ~/.pc_bash_utils/python.sh
-source ~/.pc_bash_utils/cd.sh
-source ~/.pc_bash_utils/tmp.sh
-source ~/.pc_bash_utils/kubectl.sh
-source ~/.pc_bash_utils/git.sh
-source ~/.pc_bash_utils/openssl.sh
-source ~/.pc_bash_utils/ptmux.sh
-source ~/.pc_bash_utils/date_utils.sh
-source ~/.pc_bash_utils/copy.sh
-source ~/.pc_bash_utils/docker.sh
-source ~/.pc_bash_utils/exec-bash-script.sh
-source ~/.pc_bash_utils/conversion.sh
-source ~/.pc_bash_utils/general_problems.sh
-source ~/.pc_bash_utils/configs/init.sh
-source ~/.pc_bash_utils/utils.sh
-source ~/.pc_bash_utils/ssh.sh
-source ~/.pc_bash_utils/vars.sh
+  source ~/.pc_bash_utils/eval.sh
+  source ~/.pc_bash_utils/default-options-for-commands.sh
+  source ~/.pc_bash_utils/python.sh
+  source ~/.pc_bash_utils/cd.sh
+  source ~/.pc_bash_utils/tmp.sh
+  source ~/.pc_bash_utils/kubectl.sh
+  source ~/.pc_bash_utils/git.sh
+  source ~/.pc_bash_utils/openssl.sh
+  source ~/.pc_bash_utils/ptmux.sh
+  source ~/.pc_bash_utils/date_utils.sh
+  source ~/.pc_bash_utils/copy.sh
+  source ~/.pc_bash_utils/docker.sh
+  source ~/.pc_bash_utils/exec-bash-script.sh
+  source ~/.pc_bash_utils/conversion.sh
+  source ~/.pc_bash_utils/general_problems.sh
+  source ~/.pc_bash_utils/configs/init.sh
+  source ~/.pc_bash_utils/utils.sh
+  source ~/.pc_bash_utils/ssh.sh
+  source ~/.pc_bash_utils/vars.sh
+else
+  echo "pc_bash_utils requires bash version at 5 or greater'
+fi

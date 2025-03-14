ffc0257fb096208759375f56317e05741091c169
Parveen Chahal <mail.chahal@gmail.com>
2025-03-13 23:21:11 -0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,6 +1,10 @@
-complete -W "-u --user -h --host -e --exit-master-connection -p --x11" pssh
+complete -W "-u --user -h --host -e --exit-master-connection -p --x11 --verbose" pssh
 function pssh() {
   local args=( "$@" )
+
+  local verbose="false"
+  pbash.args.is_switch_arg_enabled -l verbose -r args -- "${args[@]}" && verbose="true"
+  
   local ssh_args=()
 
   local u=""
@@ -22,6 +26,10 @@ function pssh() {
   
   pbash.args.is_switch_arg_enabled -s e -l exit-master-connection -r args -- "${args[@]}" && ssh_args+=( -O exit )
   pbash.args.is_switch_arg_enabled -l x11 -r args -- "${args[@]}" && ssh_args+=( -o "ForwardAgent yes" -Y -C )
+  if [ "$verbose" == "true" ]; then
+    pbu.eval.cmd_with_echo ssh "${ssh_args[@]}" "${args[@]}"
+  else
+    pbu.eval.cmd ssh "${ssh_args[@]}" "${args[@]}"
+  fi
   
-  pbu.eval.cmd_with_echo ssh "${ssh_args[@]}" "${args[@]}"
 }

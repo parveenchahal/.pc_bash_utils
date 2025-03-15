c8ee5222332d85a00ea5d54b092d94c47b0c52c7
pc <pc@vbox.hsd1.wa.comcast.net>
2025-03-14 17:39:11 -0700

    Updated pc_bash_utils

--- internal/array.sh
+++
@@ -1,8 +0,0 @@
-function pbu.array.size {
-  printf "${#@}"
-}
-
-function pbu.array.print {
-  [ "$#" != "0" ] && printf "['%s']" "$(pbu.string.join "', '" "$@")" && return
-  printf "[]"
-}
--- internal/checks.sh
+++
@@ -1,4 +0,0 @@
-function pbu.is_macos() {
-  [[ "$OSTYPE" == "darwin"* ]] || return 1
-  return 0
-}

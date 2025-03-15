ea5f00706b3b47e9c1ff70275295f18429d03a60
Parveen Chahal <hello@parveen.me>
2025-03-15 05:02:08 -0700

    Updated pc_bash_utils

--- pssh/init.sh
+++ pssh/init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-__pbu_install "$HOME/.pc_bash_utils/pssh"
+__pbu_update_triggered && {
+  __pbu_install "$HOME/.pc_bash_utils/pssh"
+}
 
 complete -W "--escape_args -u --user -h --host -e --exit-master-connection -p --x11 --verbose" pssh

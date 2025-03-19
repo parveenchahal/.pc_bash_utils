02e79bd5edc40a20db1a143e4d79b0c64be9156b
Parveen Chahal <mail.chahal@gmail.com>
2025-03-19 02:22:55 -0700

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-complete -W "--set_prefix --get_prefix --var" vars.create
+complete -W "--setter_prefix --getter_prefix --var" vars.create
 function vars.create() {
   [ -d "$(pbu_data_path)" ] || mkdir "$(pbu_data_path)"
   local var
   local vars=()
   local args=()
-  local set_prefix=''
-  local get_prefix=''
+  local setter_prefix=''
+  local getter_prefix=''
 
-  pbash.args.extract -l set_prefix: -o set_prefix -r args -- "$@" || { pbu.errors.echo "--set_prefix is required."; return 1; }
-  pbash.args.extract -l get_prefix: -o get_prefix -r args -- "$@" || { pbu.errors.echo "--get_prefix is required." ; return 1; }
+  pbash.args.extract -l setter_prefix: -o setter_prefix -r args -- "$@" || { pbu.errors.echo "--setter_prefix is required."; return 1; }
+  pbash.args.extract -l getter_prefix: -o getter_prefix -r args -- "$@" || { pbu.errors.echo "--getter_prefix is required." ; return 1; }
   pbash.args.extract -l var: -o vars -- "${args[@]}" || { pbu.errors.echo "--var is required." ; return 1; }
 
   for var in ${vars[@]}; do
-    local var_setter="${set_prefix}${var}"
-    local var_getter="${get_prefix}${var}"
+    local var_setter="${setter_prefix}${var}"
+    local var_getter="${getter_prefix}${var}"
     echo "#!/usr/bin/env bash" > "/tmp/$var_setter"
     echo "echo "\$1" > $(pbu_data_path)/vars.${var}" >> "/tmp/$var_setter"
     echo "echo \'\$1\' is set in ${var}. Use \'$var_getter\' command to get the value." >> "/tmp/$var_setter"
@@ -53,22 +53,22 @@ function vars.reset() {
   done
 }
 
-complete -W "--set_prefix --get_prefix --var" vars.delete
+complete -W "--setter_prefix --getter_prefix --var" vars.delete
 function vars.delete() {
   [ -d "$(pbu_data_path)" ] || mkdir "$(pbu_data_path)"
   local var
   local vars=()
   local args=()
-  local set_prefix=''
-  local get_prefix=''
+  local setter_prefix=''
+  local getter_prefix=''
 
-  pbash.args.extract -l set_prefix: -o set_prefix -r args -- "$@" || { pbu.errors.echo "--set_prefix is required."; return 1; }
-  pbash.args.extract -l get_prefix: -o get_prefix -r args -- "$@" || { pbu.errors.echo "--get_prefix is required." ; return 1; }
+  pbash.args.extract -l setter_prefix: -o setter_prefix -r args -- "$@" || { pbu.errors.echo "--setter_prefix is required."; return 1; }
+  pbash.args.extract -l getter_prefix: -o getter_prefix -r args -- "$@" || { pbu.errors.echo "--getter_prefix is required." ; return 1; }
   pbash.args.extract -l var: -o vars -- "${args[@]}" || { pbu.errors.echo "--var is required." ; return 1; }
 
   for var in ${vars[@]}; do
-    local var_setter="${set_prefix}${var}"
-    local var_getter="${get_prefix}${var}"
+    local var_setter="${setter_prefix}${var}"
+    local var_getter="${getter_prefix}${var}"
     [ -f "$(pbu_bin_path)/$var_setter" ] && { echo "Deleting '$(pbu_bin_path)/$var_setter'"; rm -f "$(pbu_bin_path)/$var_setter"; }
     [ -f "$(pbu_bin_path)/$var_getter" ] && { echo "Deleting '$(pbu_bin_path)/$var_getter'"; rm -f "$(pbu_bin_path)/$var_getter"; }
     [ -f "$(pbu_data_path)/vars.$var" ] && { echo "Deleting '$(pbu_data_path)/vars.$var'"; rm -f "$(pbu_data_path)/vars.$var"; }

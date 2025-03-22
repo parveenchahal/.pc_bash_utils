97c0d23f996f0493e678d0cb10dcdc7a89606ca2
Parveen Chahal <hello@parveen.me>
2025-03-22 14:14:09 -0700

    Updated pc_bash_utils

--- internal/eval/_complete.sh
+++ internal/eval/_complete.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-complete -W "-c --confirmation -v --verbose" pbu.eval
+complete -W "-c --confirmation -v --verbose --serialized_args" pbu.eval
 complete -W "--lock_id --timeout" pbu.eval.with_lock
